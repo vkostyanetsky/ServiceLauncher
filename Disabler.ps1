be171a213ebfb29fd4011e85e1596990efbd9ba0
Vlad Kostyanetsky <vlad@kostyanetsky.ru>
2022-02-02 22:58:17 +0700

    Добавил скрипт-отключалку и описание для него

---
+++ Disabler.ps1
@@ -0,0 +1,34 @@
+Param (
+    [switch]$elevated,
+    [string]$path
+)
+
+function Test-Admin {
+
+    $currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
+    $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+    
+}
+
+# check for privileges
+
+If ((Test-Admin) -eq $false) {
+
+    If ($elevated) {
+    
+        # tried to elevate, did not work, aborting
+        
+    } 
+    Else {
+            
+        Start-Process powershell.exe -Verb RunAs -ArgumentList ('-noprofile -noexit -file "{0}" -elevated -path {1}' -f ($myinvocation.MyCommand.Definition, $path))
+        
+    }
+
+    exit
+    
+}
+
+# set manual mode for an each service given
+
+Get-Content $path | Get-Service -Name {$_} | Where-Object {$_.StartupType -ne 'Manual'} | Set-Service -StartupType Manual -PassThru
